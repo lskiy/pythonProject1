c62e5245c320b56066c18151f63f0a90055039d6
lskiy <1624667528@qq.com>
2022-06-23 16:43:49 +0800

    Changes

--- main.py
+++ main.py
@@ -14,22 +14,27 @@ def request_dangdang(url):
 
 def save_to_excel(soup):
     li_list = soup.find(class_='bang_list clearfix bang_list_mode').find_all('li')
-    try:
-        for li in li_list:
-            list_num = li.find('div').text  #1.
-            img = li.find(class_="pic").find('a').find('img').get('src')    #http://img3m4.ddimg.cn/0/32/29276874-1_l_11.jpg
-            name = li.find(class_="name").find('a').text    #外婆出租中
-            pls = li.find(class_="star").find('a').text #52256条评论
-            title = li.find_all(class_="publisher_info")[0].find('a').get('title') #[英]丽芙·阿巴思诺特 ,周唯 译,酷威文化 出品
-            cbsj = li.find_all(class_="publisher_info")[1].find('span').text #2021-07-01
-            cbs = li.find_all(class_="publisher_info")[1].find('a').text #四川文艺出版社
-            biaosheng=li.find(class_="biaosheng").find('span').text #22985次
-            price_n=li.find(class_="price_n").text#¥21.00
-            price_r=li.find(class_="price_r").text#¥42.00
-            price_s=li.find(class_="price_s").terxt#5.0折
-            price_e_n = li.find(class_="price_e").find(class_="price_n").text#¥7.98
-    except AttributeError:
-        return None
+    print(li_list.__len__())
+    for li in li_list:
+        list_num = li.find('div').text  #1.
+        img = li.find(class_="pic").find('a').find('img').get('src')    #http://img3m4.ddimg.cn/0/32/29276874-1_l_11.jpg
+        name = li.find(class_="name").find('a').text    #外婆出租中
+        pls = li.find(class_="star").find('a').text #52256条评论
+        title = li.find_all(class_="publisher_info")[0].find('a').get('title') #[英]丽芙·阿巴思诺特 ,周唯 译,酷威文化 出品
+        cbsj = li.find_all(class_="publisher_info")[1].find('span').text #2021-07-01
+        cbs = li.find_all(class_="publisher_info")[1].find('a').text #四川文艺出版社
+        biaosheng=li.find(class_="biaosheng").find('span').text #22985次
+        price_n=li.find(class_="price_n").text#¥21.00
+        price_r=li.find(class_="price_r").text#¥42.00
+        price_s=li.find(class_="price_s").terxt#5.0折
+        price_e_n = li.find(class_="price_e").find(class_="price_n")#¥7.9
+        if price_e_n is not None:
+            price_e_n=price_e_n.text
+        print(price_e_n)
+
+
+
+
 
 
 
